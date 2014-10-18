c9918950667dc2c01c7ac1b1fc5bb7280b1b69b0
Mehael <mehael@mail.ru>
2014-10-18 11:42:54 +1100

    Flask GetPost Example

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+	
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    error = None
+    if request.method == 'POST':
+        return "post"
+    else:
+        return "get"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
