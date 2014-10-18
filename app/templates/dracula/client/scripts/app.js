6eb1ef2c2bbec5dfa2470d6eb53a19938a4f098a
Даниил Пронин <mail@grawl.ru>
2014-10-19 01:50:01 +1100

    results init

--- app/templates/dracula/client/scripts/app.js
+++ app/templates/dracula/client/scripts/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 	$('a[href^="http"]').attr('target','_blank');
+	$('.js-search-submit').on('click', function(){
+		var spawnpoint=$('.js-results-spawn'),
+			list_wrapper='ul',
+			list_item_class='results-item',
+			list_class=list_item_class+'s';
+		spawnpoint.append('<'+list_wrapper+'>');
+		for(var i=0; i<testdata.length; ++i) {
+			var item=testdata[i]
+			//console.log('part: '+item);
+			spawnpoint.find(list_wrapper)
+				.addClass(list_class)
+				.append(''
+					+'<li class="'
+						+list_item_class
+					+' secondary radius label" data-weight='
+						+item.weight
+					+' data-source='
+						+item.source
+					+'>'
+					+item.title)
+		}
 	});
+});
