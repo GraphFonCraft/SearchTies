350da4bf007795435df5cde67e8feac9e82bc09d
Red-T-Shirt <belotskayamailbox@gmail.com>
2014-10-18 11:20:29 +1100

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,5 +4,7 @@ itemlist1 = xmldoc.getElementsByTagName('Adj')
 itemlist2 = xmldoc.getElementsByTagName('Noun')
 
 
+output = []
+
 for i in range(len(itemlist2)):
-    print itemlist1[i].attributes['val'].value + " " + itemlist2[i].attributes['val'].value
+    output.append(itemlist1[i].attributes['val'].value + " " + itemlist2[i].attributes['val'].value)
