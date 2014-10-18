392379e6fc8b6c4c10982583a189f570c1a77b2b
Даниил Пронин <mail@grawl.ru>
2014-10-19 06:26:50 +1100

    restyle!

--- app/templates/dracula/client/scripts/app.js
+++ app/templates/dracula/client/scripts/app.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
 	$('a[href^="http"]').attr('target','_blank');
 	var class_results_shown='results-arrived';
-	$('.js-search-submit').on('click', function(){
-		var spawnpoint=$('.js-results-spawn');
-		spawnpoint.jQCloud(testdata);
-		$('.search-wrapper-target').find('input').val(testdata_query);
+	var spawnpoint=$('.js-results-spawn');
+	search_submit=function(key){
+		var testdata_id;
+		switch(key) {
+			case 0:
+				testdata_id = testdata0;
+				break;
+			case 1:
+				testdata_id = testdata1;
+				break;
+			case 2:
+				testdata_id = testdata2;
+				break;
+		}
+		spawnpoint.children().remove();
+		spawnpoint.jQCloud('destroy');
+		spawnpoint.jQCloud(testdata_id);
 		$('body').addClass(class_results_shown);
+	};
+	$('.js-search-submit').on('click', function(){
+		search_submit(0);
+	});
+	$('.js-search-suggestion').on('click', function(){
+		$('.search-wrapper-target').find('input').val($(this).html());
+		search_submit($(this).data('id'));
 	});
 });
