71831e5756ca397f12b81a2a9f4fed0082f359d9
Mehael <mehael@mail.ru>
2014-10-19 02:28:42 +1100

    Human-confort tomita results

--- app/tomita/combo.cxx
+++ app/tomita/combo.cxx
@@ -1,4 +1,4 @@
 ﻿#encoding "utf-8" 
 
-Combo -> Adj<gnc-agr[1]> interp (Definition.Adj)
- Noun<gnc-agr[1], no_hom, rt> interp (Definition.Noun);
+Combo -> Adj<gnc-agr[1]> interp (Definition.Adj::not_norm)
+ Noun<gnc-agr[1], rt> interp (Definition.Noun::not_norm);
