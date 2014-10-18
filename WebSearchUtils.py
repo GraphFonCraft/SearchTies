c6f8b3829ee59c550133f79c49edf93c675842da
daeh0f <googolsoap@gmail.com>
2014-10-17 20:55:47 -0400

    some initial web search stuff

---
+++ WebSearchUtils.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
+
+def getTopDuck2GoUrls(queryString):
+    queryTemplate = u"http://duckduckgo.com/html/?q="
+    queryUrl = queryTemplate + queryString
+    site = urllib2.urlopen(urllib2.Request(queryUrl))
+    htmlResponse = site.read()
+    parsedResponse = BeautifulSoup(htmlResponse)
+
+    links = []
+    for i in parsedResponse.findAll('div', {'class': re.compile('links_main*')}):
+        links.append( i.a['href'] )
+
+    return links
+
+for link in  getTopDuck2GoUrls(u"Карова"):
+    print link
