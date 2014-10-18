15c241141e719afbdaca171322c9e670bd9cca0d
Mehael <mehael@mail.ru>
2014-10-19 00:46:49 +1100

    Finaly work in one spree with output /search?q=textToSearch

--- post_facts_parser.py
+++ post_facts_parser.py
@@ -1,11 +1,26 @@
-#coding:utf-8
-from xml.dom import minidom
-xmldoc = minidom.parse('items.xml')
-itemlist1 = xmldoc.getElementsByTagName('Adj')
-itemlist2 = xmldoc.getElementsByTagName('Noun')
+﻿from xml.dom.minidom import parse
+from sets import Set
 
+def parser(xmlfile):
+	xmldoc = parse(xmlfile)
+	itemlist1 = xmldoc.getElementsByTagName('Adj')
+	itemlist2 = xmldoc.getElementsByTagName('Noun')
+	
+	list = Set()
+	for i in range(len(itemlist1)):
+		if i!= (len(itemlist1)-1):
+			str = '"' + (itemlist1[i].attributes['val'].value).encode('utf-8') + " " + (itemlist2[i].attributes['val'].value).encode('utf-8') + '"'
+			list.add(str)
+	
+	need_output = '{"id100" : {"source" : "http://" : [\n'
+	i = 0
+	for item in list:
+		i+=1
+		if i != len(list):
+			need_output += item + ',\n '
+		else:
+			need_output += item  + '\n]}}'	
+	
+	return need_output
 
-output = []
-
-for i in range(len(itemlist2)):
-    print itemlist1[i].attributes['val'].value + " " + itemlist2[i].attributes['val'].value
+#print(parser("/srv/http/app/facts/text"))
